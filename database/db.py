c0e1f35ceef1bff754a9de93459a294cd3a7d60c
SM - BOTS <surajmain1204@gmail.com>
2026-06-03 15:00:04 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -3,76 +3,58 @@
 # Ask Doubt on telegram @KingVJ01
 
 import motor.motor_asyncio
-from config import DB_NAME, DB_URI
+from config import DB_URI, DB_NAME
 
 class Database:
     
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.users
-
-    def new_user(self, id, name):
+        self.col = self.db.user
+        
+    def new_user(self, id):
         return dict(
-            id = id,
-            name = name,
-            session = None,
-            api_id = None,
-            api_hash = None,
+            id=id,
+            join_date=None,
+            upload_count=0,
+            dump_channel=None
         )
-    
-    async def add_user(self, id, name):
-        user = self.new_user(id, name)
+        
+    async def add_user(self, id):
+        user = self.new_user(id)
         await self.col.insert_one(user)
-    
+        
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
-        return bool(user)
-    
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+        
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-
+        
     async def get_all_users(self):
-        return self.col.find({})
-
+        all_users = self.col.find({})
+        return all_users
+        
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
 
-    async def set_session(self, id, session):
-        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+# Database client instance ka setup
+db = Database(DB_URI, DB_NAME)
 
-    async def get_session(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('session')
-
-    async def set_api_id(self, id, api_id):
-        await self.col.update_one({'id': int(id)}, {'$set': {'api_id': api_id}})
-
-    async def get_api_id(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('api_id')
-
-    async def set_api_hash(self, id, api_hash):
-        await self.col.update_one({'id': int(id)}, {'$set': {'api_hash': api_hash}})
-
-    async def get_api_hash(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('api_hash')
-
-db = Database(DB_URI, "TechVJDemoBot")
-
-# --- DUMP CHANNEL DATABASE CODE ---
-
-async def set_dump_channel(user_id, channel_id):
-    """User ki dump channel ID database me save karne ke liye"""
-    await db.users.update_one(
-        {"_id": int(user_id)},
-        {"$set": {"dump_channel": int(channel_id)}},
-        upsert=True
-    )
+# ----------------------------------------------------
+# DUMP CHANNEL DATABASE FUNCTIONS BY EVAROSE
+# ----------------------------------------------------
 
 async def get_dump_channel(user_id):
-    """User ki dump channel ID database se nikalne ke liye"""
-    user = await db.users.find_one({"_id": int(user_id)})
-    return user.get("dump_channel", None) if user else None
+    user = await db.col.find_one({"id": int(user_id)})
+    if user:
+        return user.get("dump_channel")
+    return None
+
+async def set_dump_channel(user_id, channel_id):
+    await db.col.update_one(
+        {"id": int(user_id)},
+        {"$set": {"dump_channel": channel_id}},
+        upsert=True
+    )
