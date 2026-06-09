9f0891ed0b2a8302a25074a1a83918e3575f7f10
SM - BOTS <surajmain1204@gmail.com>
2026-06-09 21:11:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,11 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Don't Remove Credit Tg - @EvaRoseX
+import pyromod
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("EvaRose", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-	TechVJUser.start()
+    # 🌟 CHANGE HERE TO EVAROSEUSER
+    EvaRoseUser = Client("EvaRose", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+    EvaRoseUser.start()
 else:
-    TechVJUser = None
-
-class Bot(Client):
-
-    def __init__(self):
-        super().__init__(
-            "evarose login",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            plugins=dict(root="EvaRose"),
-            workers=150,
-            sleep_threshold=5
-        )
-
-    async def start(self):
-        await super().start()
-        print('Bot Started Powered By @EvaRoseX')
-
-    async def stop(self, *args):
-        await super().stop()
-        print('Bot Stopped Bye')
-
-if __name__ == "__main__":
-    bot = Bot()
-    bot.run()
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    EvaRoseUser = None
