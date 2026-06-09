a62643f369a6d0cdb75c4fd4077382d03b031981
SM - BOTS <surajmain1204@gmail.com>
2026-06-09 20:31:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-import pyromod # Bas shuru me import hona kaafi hai
-
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-    TechVJUser = Client("EvaRose", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-    TechVJUser.start()
+	TechVJUser = Client("EvaRose", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+	TechVJUser.start()
 else:
     TechVJUser = None
 
